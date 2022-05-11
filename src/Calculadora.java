d58bca3cc73bd1b41a1979976663a353ccbf8eff
Jonas-Flores <jonas-silva8@outlook.com>
2022-05-11 09:54:57 -0300

    add tangente e %

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,14 +5,14 @@ import java.text.DecimalFormat;
 
 public class Calculadora extends Frame implements ActionListener {
 
-    Label L1, L2, L3, L4, L5;
-    Button B1, B2, B3, B4, B5, B6, B7, B8, B9, B10;
-    TextField T1, T2, T3, T4, T5;
+    Label L1, L2, L3, L4, L5, L6;
+    Button B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12;
+    TextField T1, T2, T3, T4, T5, T6;
 
     public static void main(String args[])
     {
         Calculadora Janela=new Calculadora();
-        Janela.addWindowListener (new fechajanela());
+        //Janela.addWindowListener (new fechajanela());
         Janela.show();
     }
 
@@ -22,7 +22,7 @@ public class Calculadora extends Frame implements ActionListener {
         setSize(200,300);
         setLocation(50,50);
         setBackground(new Color(1,10,50));
-        setLayout(new GridLayout(10,2));
+        setLayout(new GridLayout(12,2));
 
         L1 = new Label("num.1");
         L1.setForeground(Color.white);
@@ -32,6 +32,8 @@ public class Calculadora extends Frame implements ActionListener {
         L4.setForeground(Color.white);
         L5 = new Label("cos");
         L5.setForeground(Color.white);
+        L6 = new Label("tan");
+        L6.setForeground(Color.white);
         L3 = new Label("Total");
         L3.setForeground(Color.white);
 
@@ -40,6 +42,7 @@ public class Calculadora extends Frame implements ActionListener {
         L3.setFont(new Font("",Font.BOLD,14));
         L4.setFont(new Font("",Font.BOLD,14));
         L5.setFont(new Font("",Font.BOLD,14));
+        L6.setFont(new Font("",Font.BOLD,14));
 
         B1 = new Button ("+"); B1.addActionListener(this);
         B2 = new Button ("-"); B2.addActionListener(this);
@@ -49,6 +52,8 @@ public class Calculadora extends Frame implements ActionListener {
         B8 = new Button("sqrt"); B8.addActionListener(this);
         B9 = new Button("sin"); B9.addActionListener(this);
         B10 = new Button("cos"); B10.addActionListener(this);
+        B11 = new Button ("tan"); B11.addActionListener(this);
+        B12 = new Button ("%"); B12.addActionListener(this);
         B5 = new Button ("Limpar"); B5.addActionListener(this);
         B6 = new Button ("Sair"); B6.addActionListener(this);
 
@@ -62,13 +67,15 @@ public class Calculadora extends Frame implements ActionListener {
         T3 = new TextField();
         T4 = new TextField();
         T5 = new TextField();
+        T6 = new TextField();
 
         T3.setEditable(false); //define que o textField como somente leitura
         T4.setEditable(false);
         T5.setEditable(false);
+        T6.setEditable(false);
 
-        add(L1); add(T1); add(L2); add(T2); add(L4); add(T4); add(L5); add(T5); add(L3); add(T3);
-        add(B1); add(B2); add(B3); add(B4); add(B7); add(B8); add(B9); add(B10);
+        add(L1); add(T1); add(L2); add(T2); add(L4); add(T4); add(L5); add(T5); add(L6); add(T6); add(L3); add(T3);
+        add(B1); add(B2); add(B3); add(B4); add(B7); add(B8); add(B9); add(B10); add(B11); add (B12);
         add(B5); add(B6);
     }
 
@@ -80,6 +87,7 @@ public class Calculadora extends Frame implements ActionListener {
             T2.setText("");
             T4.setText("");
             T5.setText("");
+            T6.setText("");
             T3.setText("");
             return;
         }
@@ -90,18 +98,19 @@ public class Calculadora extends Frame implements ActionListener {
             T2.setText("");
             T4.setText("");
             T5.setText("");
+            T6.setText("");
             T3.setText("");
             System.exit(0);
             return;
         }
 
-        double N1, N2, result=0;
+        double N1, N2=0, result=0;
 
 
         try
         {
             N1 = Double.parseDouble(T1.getText());
-            if (e.getSource()==B8 || e.getSource()==B9 || e.getSource()==B10){
+            if (e.getSource()==B8 || e.getSource()==B9 || e.getSource()==B10 || e.getSource () == B11){
                 N2 = 0;
             }else
                 N2 = Double.parseDouble(T2.getText());
@@ -111,6 +120,8 @@ public class Calculadora extends Frame implements ActionListener {
             T3.setText("Erro");
             T4.setText("Erro");
             T5.setText("Erro");
+            T5.setText("Erro");
+            T6.setText("Erro");
             return;
         }
 
@@ -157,5 +168,18 @@ public class Calculadora extends Frame implements ActionListener {
             T5.setText(""+deci.format(result));
         }
 
+        if (e.getSource()==B11) {
+            result = Math.tan(N1);
+            DecimalFormat deci = new DecimalFormat("0.0000000");
+            T6.setText(""+deci.format(result));
+        }
+
+        if (e.getSource()==B12) {
+            result = N1 *(N2/100);
+            T3.setText(""+result);
+        }
+
+
     }
 }
+
